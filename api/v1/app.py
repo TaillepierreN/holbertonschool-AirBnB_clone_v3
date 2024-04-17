2e418d8921b2eea16662a9b793e90c7728bf1bb5
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-17 11:52:02 +0200

    added api files

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""web app"""
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close db"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', Threaded=True)
