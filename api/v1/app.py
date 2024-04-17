fdca1dd3f9a66995aace92f0da9163eae1a05554
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-17 11:57:37 +0200

    added comments to api files / fix host and port of app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,4 +16,6 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', Threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port), Threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""init file"""
 from flask import Blueprint
 from api.v1.views.index import *
 
