dbc4ed37b7bcfac5369d3272b2bdfce7197f5666
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-19 08:56:23 +0200

    missing slash in places_reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,7 +8,7 @@ from models.review import Review
 from models.user import User
 
 
-@app_views.route("places/<place_id>/reviews",
+@app_views.route("/places/<place_id>/reviews",
                  strict_slashes=False, methods=["GET"])
 def get_reviews(place_id):
     """Retrieve all reviews for a place    """
