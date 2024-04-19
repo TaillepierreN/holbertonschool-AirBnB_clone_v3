2ebb7af0c5b1fb2fec13638df8424cee41be8d75
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-19 10:07:44 +0200

    change delete method of state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,7 +34,7 @@ def delete_state(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    storage.delete(state)
+    state.delete()
     storage.save()
     return jsonify({}), 200
 
